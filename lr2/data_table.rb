ba931a30bf0e3beaaa18d97ffe45100d078d6b88
krissstyu02 <kristi090217@gmail.com>
2023-03-17 20:52:42 +0300

    добавлены методы в DataTable

--- lr2/data_table.rb
+++ lr2/data_table.rb
@@ -1,12 +1,33 @@
 
 class DataTable
-def initialize(table)
-  self.table = table
+
+  attr_reader :rows_count, :cols_count
+
+  def initialize(table)
+    self.rows_count = table.length
+    max_cols = 0
+    table.each do |row|
+      max_cols = row.length if row.size > max_cols
+    end
+    self.cols_count = max_cols
+    self.table = table
+  end
+
+  #метод позволяющиq – получить элемент по номеру строки и столбца
+  def get_item(row, col)
+    return nil if row >= rows_count || row.negative?
+    return nil if col >= cols_count || col.negative?
+
+    table[row][col]
+  end
+
+  def to_s
+    table.map { |row| "[#{row.join(', ')}]" }.join("\n")
+  end
+
+  private
+
+  attr_accessor :table
+  attr_writer :rows_count, :cols_count
+
 end
-
-private
-attr_accessor :table
-
-end
-
-
