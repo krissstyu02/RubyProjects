9b29c2445ae39d124766c14df27ef85362fb51ad
krissstyu02 <kristi090217@gmail.com>
2023-03-17 21:38:51 +0300

    создан класс data_list_student_short.rb

--- lr2/data_list.rb
+++ lr2/data_list.rb
@@ -1,43 +1,29 @@
-class DataList
+class Data_list
 
-  private_class_method :new
+  attr_reader :selected
 
+  def initialize(data)
+    @data = data
+    @selected = []
+  end
 
-
-  def initialize(objects)
-    self.objects_list = objects
-    self.selected_objects = []
+  def [](index)
+    @data[index]
   end
 
   def select(number)
-    selected_objects.append(number)
-  end
-
-  def clear_select
-    self.selected_objects = []
+    @selected << number
   end
 
   def get_selected
-    return [] if selected_objects.empty?
-
-    selected_id_list = []
-    selected_objects.each do |num|
-      selected_id_list.append(objects_list[num].id)
-    end
-    selected_id_list
+    @selected
   end
-
-  protected
 
   def get_names
     raise NotImplementedError, "get_names нужно реализовать в классе наследнике"
   end
 
   def get_data
-    raise NotImplementedError, "get_data нужно реализовать в классе наследнике"
+    raise NotImplementedError, "get_data must нужно реализовать в классе наследнике"
   end
-
-  private
-
-  attr_accessor :selected_objects, :objects_list
 end
