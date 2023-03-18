8684b5f75197989a0952fb18afffceea295c896e
krissstyu02 <kristi090217@gmail.com>
2023-03-18 09:50:30 +0300

    класс data_list_student_short.rb

---
+++ lr2/data_list_student_short.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require_relative 'dataTable'
+require_relative 'DataList'
+class DataListStudentShort < DataList
+  public_class_method :new
+
+  def initialize(objects)
+    super(objects)
+  end
+
+  def get_names
+    ["last_name_and_initials", "git", "contact"]
+  end
+  def get_data
+    result = []
+    id = 0
+    objects_list.each do |object|
+      row = []
+      row << id
+      row << object.last_name_and_initials
+      row << object.git
+      row << object.contact
+      result << row
+      id += 1
+    end
+    DataTable.new(result)
+  end
+end
