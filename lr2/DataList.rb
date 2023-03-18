9b93ff7cf48f2fc032b4386e2e1b9af0b6881383
krissstyu02 <kristi090217@gmail.com>
2023-03-18 09:41:55 +0300

    класс DataList

---
+++ lr2/DataList.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+class DataList
+
+  private_class_method :new
+
+  def initialize(objects)
+    self.objects_list = objects
+    self.selected_objects = []
+  end
+
+  def select(number)
+    selected_objects.append(number)
+  end
+
+  def clear_select
+    self.selected_objects = []
+  end
+
+  def get_selected
+    return [] if selected_objects.empty?
+
+    selected_id_list = []
+    selected_objects.each do |num|
+      selected_id_list.append(objects_list[num].id)
+    end
+    selected_id_list
+  end
+
+  protected
+
+  def get_names; end
+
+  def get_data; end
+
+  private
+
+  attr_accessor :selected_objects, :objects_list
+end
