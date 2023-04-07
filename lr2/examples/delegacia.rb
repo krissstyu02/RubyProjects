0bbf83664db36bfb774718ce0a43f76c3ea2db10
krissstyu02 <kristi090217@gmail.com>
2023-04-07 21:39:04 +0300

    подключение к mysql и создание таблицы

--- lr2/examples/delegacia.rb
+++ lr2/examples/delegacia.rb
@@ -1,2 +1,46 @@
+class Bird
+  def initialize
+    @is_alive = true
+  end
+
+  def walk
+    puts "I'm walking."
+  end
+end
+
+class Flyer
+  def fly
+    puts "I'm flying!"
+  end
+end
+
+class Eagle
+  def initialize
+    @bird = Bird.new
+    @flyer = Flyer.new
+  end
+
+  def do_something
+    if @bird.is_alive
+      @flyer.fly
+    else
+      puts "I'm dead."
+    end
+  end
+end
+
+class Penguin
+  def initialize
+    @bird = Bird.new
+  end
+
+  def do_something
+    if @bird.is_alive
+      @bird.walk
+    else
+      puts "I'm dead."
+    end
+  end
+end
 # frozen_string_literal: true
 
