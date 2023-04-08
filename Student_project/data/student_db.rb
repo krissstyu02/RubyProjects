c4a396444d19c857e7e54bf3cef707768aa41e6b
krissstyu02 <kristi090217@gmail.com>
2023-04-08 09:03:46 +0300

    тест и исправление ошибок student_db.rb

--- Student_project/data/student_db.rb
+++ Student_project/data/student_db.rb
@@ -1,6 +1,8 @@
 
 require 'mysql2'
 require_relative '../models/student'
+require_relative '../models/student_short'
+require_relative '../data/containers/data_list_student_short'
 
 class StudentListDB
   def initialize(db_config)
@@ -8,14 +10,13 @@ class StudentListDB
   end
 
   def student_by_id(student_id)
-    client.query("SELECT * FROM students WHERE id = #{student_id}").first.tap do |result|
-      return Student.from_hash(result) if result
-    end
+    result = client.query("SELECT * FROM students WHERE id = #{student_id}").first
+    return Student.from_hash(result.transform_keys(&:to_sym)) if result
     nil
   end
 
   def add_student(student)
-    stmt = client.prepare('INSERT INTO students (first_name, last_name, paternal_name, phone, telegram, mail, git) VALUES (?, ?, ?, ?, ?, ?, ?)')
+    stmt = client.prepare('INSERT INTO students (first_name, last_name, paternal_name, phone, telegram, email, git) VALUES (?, ?, ?, ?, ?, ?, ?)')
     stmt.execute(*student_fields(student))
     client.last_id
   end
@@ -25,7 +26,7 @@ class StudentListDB
   end
 
   def replace_student(student_id, student)
-    stmt = client.prepare('UPDATE students SET first_name = ?, last_name = ?, paternal_name = ?, phone = ?, telegram = ?, mail = ?, git = ? WHERE id = ?')
+    stmt = client.prepare('UPDATE students SET first_name = ?, last_name = ?, paternal_name = ?, phone = ?, telegram = ?, email = ?, git = ? WHERE id = ?')
     stmt.execute(*student_fields(student), student_id)
   end
 
@@ -33,8 +34,9 @@ class StudentListDB
     client.query('SELECT COUNT(id) FROM students').first.values.first
   end
 
-  def get_k_n_student_short_list(k, n)
-    students = client.query("SELECT * FROM students LIMIT #{n} OFFSET #{(k-1)*n}")
+
+  def get_k_n_student_short_list(k,n)
+    students = client.prepare('SELECT * FROM students LIMIT ? OFFSET ?').execute((k-1)*n,n)
     slice = students.map { |h| StudentShort.new(Student.from_hash(h)) }
     DataListStudentShort.new(slice)
   end
@@ -44,7 +46,7 @@ class StudentListDB
   attr_accessor :client
 
   def student_fields(student)
-    [student.first_name, student.last_name, student.paternal_name, student.phone, student.telegram, student.mail, student.git]
+    [student.first_name, student.last_name, student.paternal_name, student.phone, student.telegram, student.email, student.git]
   end
 end
 
@@ -58,5 +60,11 @@ db_config = {
 db = StudentListDB.new(db_config)
 puts db.student_count
 
+student1 = Student.new('Гавриш', 'Геннадий', 'Алексеевич')
+# db.add_student(student1)
+puts db.student_by_id(1)
+# puts db.remove_student(3)
+# db.replace_student(2,student1)
+# puts db.get_k_n_student_short_list(1,2)
 
 
